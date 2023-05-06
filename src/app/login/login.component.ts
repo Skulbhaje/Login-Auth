b5f9f0161726cd9a0e19b709515c40c3d64b0a24
Shailendra <shailendrakulbhaje@Shailendras-MacBook-Pro.local>
2023-05-06 16:13:14 +0530

    login form created

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  userData: any;
+
+  constructor(private formBuilder: FormBuilder, private toastr: ToastrService,
+    private authService: AuthService, private router: Router){
+
+  }
+
+  loginForm = this.formBuilder.group({
+    username: this.formBuilder.control('', Validators.required),
+    password: this.formBuilder.control('', Validators.compose([Validators.required, Validators.pattern('')]))
+  })
+
+  proceedLogin(){
+    if(this.loginForm.valid){
+      this.authService.getByCode(this.loginForm.value.username).subscribe(res => {
+      this.userData = res;
+      console.log(this.userData);
+      if (this.userData.password == this.loginForm.value.password && this.userData.id == this.loginForm.value.username){
+        if(this.userData.isActive){
+            sessionStorage.setItem('username', this.userData.id);
+            sessionStorage.setItem('userRole', this.userData.role);
+            this.router.navigate(['']);
+        } else {
+          this.toastr.warning('This user is inactive, please contact system admin')
+        }
+      } else {
+          this.toastr.warning('Invalid Credentials!')
+      }
+      });
+    } else {
+      this.toastr.warning('Please enter valid Credentials!')
+    }
+  }
 
 }
